5121b3fda90b5e4599887ae7c22920d7987d2342
Vincent Yang <vincent.yang@episerver.com>
2019-04-12 14:16:10 +1000

    Added Bundle and Package content type

---
+++ CommerceTraining/Models/Catalog/ShirtBundle.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.Commerce.Catalog.ContentTypes;
+using EPiServer.Commerce.Catalog.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace CommerceTraining.Models.Catalog
+{
+    [CatalogContentType(DisplayName = "ShirtBundle", GUID = "6e2a0b8d-27a8-4eaa-ad8b-7b412dc91dab", Description = "")]
+    public class ShirtBundle : BundleContent
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        [Tokenize]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
