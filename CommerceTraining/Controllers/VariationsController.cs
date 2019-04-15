c7ee511c98bba7933a822fc50b5334fca4dd422d
Vincent Yang <vincent.yang@episerver.com>
2019-04-15 22:41:41 +1000

    Completed Cart D1 exercise

--- CommerceTraining/Controllers/VariationsController.cs
+++ CommerceTraining/Controllers/VariationsController.cs
@@ -1,19 +1,47 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 using CommerceTraining.Models.Catalog;
+using CommerceTraining.Models.Pages;
 using CommerceTraining.Models.ViewModels;
 using EPiServer;
 using EPiServer.Commerce.Catalog;
 using EPiServer.Commerce.Catalog.ContentTypes;
+using EPiServer.Commerce.Order;
+using EPiServer.Core;
+using EPiServer.Globalization;
+using EPiServer.Security;
 using EPiServer.Web.Routing;
+using Mediachase.Commerce.Inventory;
+using Mediachase.Commerce.InventoryService;
+using Mediachase.Commerce.Security;
 
 namespace CommerceTraining.Controllers
 {
     public class VariationsController : CatalogControllerBase<ShirtVariation>
     {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IWarehouseRepository _warehouseRepository;
+        private readonly IInventoryService _inventoryService;
+        private readonly IOrderGroupFactory _orderGroupFactory;
+        private readonly ILineItemValidator _lineItemValidator;
+
         public VariationsController(
-            IContentLoader contentLoader, UrlResolver urlResolver, AssetUrlResolver assetUrlResolver,
-            ThumbnailUrlResolver thumbnailUrlResolver) : base(contentLoader, urlResolver, assetUrlResolver, thumbnailUrlResolver)
+            IContentLoader contentLoader,
+            UrlResolver urlResolver,
+            AssetUrlResolver assetUrlResolver,
+            ThumbnailUrlResolver thumbnailUrlResolver,
+            IOrderRepository orderRepository,
+            IWarehouseRepository warehouseRepository,
+            IInventoryService inventoryService,
+            IOrderGroupFactory orderGroupFactory,
+            ILineItemValidator lineItemValidator) : base(contentLoader, urlResolver, assetUrlResolver, thumbnailUrlResolver)
         {
+            _orderRepository = orderRepository;
+            _warehouseRepository = warehouseRepository;
+            _inventoryService = inventoryService;
+            _orderGroupFactory = orderGroupFactory;
+            _lineItemValidator = lineItemValidator;
         }
 
         public ActionResult Index(ShirtVariation currentContent)
@@ -25,6 +53,62 @@ namespace CommerceTraining.Controllers
             model.CanBeMonogrammed = currentContent.CanBeMonogrammed;
 
             return View(model);
+        }
+
+        public ActionResult AddToCart(ShirtVariation currentContent, decimal Quantity, string Monogram)
+        {
+            // ToDo: (lab D1) add a LineItem to the Cart
+            var cart = _orderRepository.LoadOrCreateCart<ICart>(PrincipalInfo.CurrentPrincipal.GetContactId(), "Default");
+
+
+            IWarehouse wh = _warehouseRepository.GetDefaultWarehouse();
+            InventoryRecord rec = _inventoryService.Get(currentContent.Code, wh.Code);
+
+            // Retrieve the shirt variation is already in the cart (if cart was loaded from the db)
+            var lineItem = cart.GetAllLineItems().FirstOrDefault(x => x.Code == currentContent.Code);
+
+            if(lineItem == null)
+            {
+                lineItem = _orderGroupFactory.CreateLineItem(currentContent.Code, cart);
+                lineItem.Quantity = Quantity;
+                cart.AddLineItem(lineItem);
+            }
+            else
+            {
+                lineItem.Quantity += Quantity;
+            }
+
+            // To hold any validation issues
+            var validationIssues = new Dictionary<ILineItem, ValidationIssue>();
+
+            var validLineItem = _lineItemValidator.Validate(lineItem, cart.MarketId, (item, issue) =>
+            {
+                validationIssues.Add(item, issue);
+            });
+
+            if (validLineItem)
+            {
+                lineItem.Properties["Monogram"] = Monogram;
+                _orderRepository.Save(cart);
+            }
+
+            // if we want to redirect
+            ContentReference cartRef = _contentLoader.Get<StartPage>(ContentReference.StartPage).Settings.cartPage;
+            CartPage cartPage = _contentLoader.Get<CartPage>(cartRef);
+            var name = cartPage.Name;
+            var lang = ContentLanguage.PreferredCulture;
+            string passingValue = cart.Name;
+
+            // go to the cart page, if needed
+            return RedirectToAction("Index", lang + "/" + name, new { passedAlong = passingValue });
+        }
+
+        public void AddToWishList(ShirtVariation currentContent)
+        {
+            ICart myWishList = _orderRepository.LoadOrCreateCart<ICart>(PrincipalInfo.CurrentPrincipal.GetContactId(), "WishList");
+            ILineItem lineItem = _orderGroupFactory.CreateLineItem(currentContent.Code, myWishList);
+            myWishList.AddLineItem(lineItem);
+            _orderRepository.Save(myWishList);
         }
     }
 }
