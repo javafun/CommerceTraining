088013e25f817ea78edb38f724f6c78b7ab7983a
Vincent Yang <vincent.yang@episerver.com>
2019-04-18 15:26:02 +1000

    Completed Module I - Search exercise

--- CommerceTraining/Controllers/SearchController.cs
+++ CommerceTraining/Controllers/SearchController.cs
@@ -1,22 +1,19 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
-using EPiServer;
-using EPiServer.Core;
-using EPiServer.Framework.DataAnnotations;
-using EPiServer.Web.Mvc;
+﻿using CommerceTraining.Models.Catalog;
 using CommerceTraining.Models.Pages;
 using CommerceTraining.Models.ViewModels;
-using EPiServer.Commerce.Catalog.ContentTypes;
-using Mediachase.Commerce.Website.Search;
-using Mediachase.Search.Extensions;
-using Mediachase.Search;
-using Mediachase.Commerce.Catalog.Objects;
-using Mediachase.Commerce.Catalog;
-using Mediachase.Commerce.Catalog.Managers;
-using System;
-using EPiServer.ServiceLocation;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Globalization;
+using EPiServer.Web.Mvc;
 using EPiServer.Web.Routing;
+using Mediachase.Commerce.Catalog;
+using Mediachase.Commerce.Website.Search;
+using Mediachase.Search;
+using Mediachase.Search.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace CommerceTraining.Controllers
 {
@@ -47,24 +44,48 @@ namespace CommerceTraining.Controllers
         public ActionResult Search(string keyWord)
         {
             // ToDo: SearchHelper and Criteria 
-
-
+            var searchFilterHelper = SearchFilterHelper.Current;
+            CatalogEntrySearchCriteria criteria = searchFilterHelper.CreateSearchCriteria(keyWord,
+                CatalogEntrySearchCriteria.DefaultSortOrder);
+            criteria.RecordsToRetrieve = 25;
+            criteria.StartingRecord = 0;
+            criteria.Locale = ContentLanguage.PreferredCulture.Name;
+            int count = 0;
+            bool cacheResult = true;
+            TimeSpan timeSpan = new TimeSpan(0, 10, 0);
             // ToDo: Search 
 
+            ISearchResults searchResult = searchFilterHelper.SearchEntries(criteria);
+            ISearchDocument aDoc = searchResult.Documents.FirstOrDefault();
 
+            int[] ints = searchResult.GetKeyFieldValues<int>();
+            List<ContentReference> refs = new List<ContentReference>();
+            ints.ToList().ForEach(i => refs.Add(_referenceConverter.GetContentLink(i, CatalogContentType.CatalogEntry, 0)));
+
+            localContent = _contentLoader.GetItems(refs, new LoaderOptions());
             // ToDo: Facets
+            List<string> facetList = new List<string>();
 
+            int facetGroups = searchResult.FacetGroups.Count();
+
+            foreach (ISearchFacetGroup item in searchResult.FacetGroups)
+            {
+                foreach (var item2 in item.Facets)
+                {
+                    facetList.Add($"{item.Name} {item2.Name} {item2.Count}");
+                }
+            }
 
             // ToDo: As a last step - un-comment and fill up the ViewModel
             var searchResultViewModel = new SearchResultViewModel();
-            /*
+
             searchResultViewModel.totalHits = new List<string> { "" }; // change
             searchResultViewModel.nodes = localContent.OfType<FashionNode>();
             searchResultViewModel.products = localContent.OfType<ShirtProduct>();
             searchResultViewModel.variants = localContent.OfType<ShirtVariation>();
             searchResultViewModel.allContent = localContent;
             searchResultViewModel.facets = facetList;
-            */
+
 
             return View(searchResultViewModel);
         }
--- CommerceTraining/Models/ViewModels/SearchResultViewModel.cs
+++ CommerceTraining/Models/ViewModels/SearchResultViewModel.cs
@@ -1,4 +1,5 @@
-﻿using EPiServer.Core;
+﻿using CommerceTraining.Models.Catalog;
+using EPiServer.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,13 @@ namespace CommerceTraining.Models.ViewModels
     {
         // no paging settings or other luxery
         // un-comment when the catalog models exist
-        /*
+
         public IEnumerable<string> totalHits { get; set; }
         public IEnumerable<FashionNode> nodes { get; set; }
         public IEnumerable<ShirtProduct> products { get; set; }
         public IEnumerable<ShirtVariation> variants { get; set; }
         public IEnumerable<IContent> allContent { get; set; }
         public IEnumerable<string> facets { get; set; }
-        */
+
     }
 }
